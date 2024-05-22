25bf8fab53249cf83a5e9a6d13f8badd2998bd0c
G85 <gabrielgg85@gmail.com>
2024-05-22 15:33:15 +0100

    G85
    Codigo corregido ... parece que guarda el contenido de las tuplas.

--- src/main.c
+++ src/main.c
@@ -23,71 +23,68 @@ int main(int argc, char *argv[])
 
 int start_zip_process(FILE* file){
 
-    //Ponemos el fichero en el principio y preparamos buffer y variable de bytes leidos
-    fseek(file,0L,SEEK_SET);
+    // Ponemos el fichero en el principio y preparamos buffer y variable de bytes leidos
+    fseek(file, 0L, SEEK_SET);
     size_t readed_bytes;
 
-
-    long buffer_size = 10; //128 * 1024;
-    printf("BUFFER SIZE: [ %lu ]\n",buffer_size);
+    long buffer_size = 10; // 128 * 1024;
+    printf("BUFFER SIZE: [ %lu ]\n", buffer_size);
     char *buffer_data = (char*)malloc(buffer_size * sizeof(char));
-    
 
-    tuple_array *_tuples_array = create_tuple_array(); //creamos objeto para guardar las tuplas
+    tuple_array *_tuples_array = create_tuple_array(); // Creamos objeto para guardar las tuplas
 
-    int first_time = 0; //variable para controlar y guardar el primer byte leido del fichero
-    size_t blocks_counter = 0; //variable para controlar los bloques que vamos leyendo, de 10 en 10 bytes (test)
+    int first_time = 0; // Variable para controlar y guardar el primer byte leido del fichero
+    size_t blocks_counter = 0; // Variable para controlar los bloques que vamos leyendo, de 10 en 10 bytes (test)
 
+    // Creamos fichero objetivo binario
+    FILE* target_file = fopen("demotext_ziped.g85", "wb");
+    if (!target_file) {
+        perror("Error al abrir el fichero objetivo");
+        free(buffer_data);
+        free(_tuples_array->tuple_list);
+        free(_tuples_array);
+        return -1;
+    }
 
-    //creamos fichero objetivo binario
-    FILE* target_file = fopen("demotext_ziped.g85","w");
- 
-    //empezamos a leer el fichero
+    // Empezamos a leer el fichero
     while ((readed_bytes = fread(buffer_data, sizeof(char), buffer_size, file)) > 0) {
 
-        unsigned long zip_data_size = (unsigned long)strlen(buffer_data);
-        printf("+: [%s] LENGTH: [%lu]\n",buffer_data,zip_data_size);
+        //if (readed_bytes < buffer_size) {
+        //    printf("REDIMENSIONANDO BUFFER_DATA\n");
+        //    buffer_data = (char*)realloc(buffer_data, readed_bytes * sizeof(char));
+        //}
 
         // ---------------- ZIP DATA -----------------------
-        add_firts_element_on_tuple_list(_tuples_array,buffer_data[0]);
-        tuple_array *_tuple_to_file = zip_data(_tuples_array , buffer_data);
+        add_firts_element_on_tuple_list(_tuples_array, buffer_data[0]);
+        tuple_array *_tuple_to_file = zip_data(_tuples_array, buffer_data);
 
-        //una vez tenemos la tupla , pasamos a guardar esta misma estructura
-        //en un fichero de forma binaria
+        // Una vez tenemos la tupla, pasamos a guardar esta misma estructura en un fichero de forma binaria
+         show_tuples_list(_tuple_to_file);
 
-        //show_tuples_list(_tuple_to_file);
+         if (_tuple_to_file) {
+            // Primero escribimos el tamaño de la estructura tuple_array
+            fwrite(&_tuple_to_file->size, sizeof(size_t), 1, target_file);
 
-        //if(_tuple_to_file){
-            //write file zipped
-            //fwrite(&_tuple_to_file,sizeof(tuple_array),1,target_file);
-        //}
+            // Luego escribimos las tuplas en sí
+            fwrite(_tuple_to_file->tuple_list, sizeof(tuple), _tuple_to_file->size, target_file);
+         }
         // ---------------- end ZIP DATA -----------------------
 
+           // UNZIP
+        //char *data_unzip = unzip_data(_tuples_array);
+        //printf("-[%s] LENGTH: [%lu]\n",data_unzip, strlen(data_unzip));
+        
 
+    } // fin while read file
 
-        if(buffer_size >= blocks_counter){
-            //fseek(file , blocks_counter , SEEK_CUR);
-            //blocks_counter = blocks_counter + buffer_size ;
-        }
-
-
-
-        // UNZIP
-        char *data_unzip = unzip_data(_tuples_array);
-        printf("-: [%s] LENGTH: [%lu]\n",data_unzip, strlen(data_unzip));
-        printf("-------------------------------------------------------\n");
-
-
-    } //fin while read file
-
-
-
-    
     fclose(file);
     fclose(target_file);
-    
+
+    free(buffer_data);
     free(_tuples_array->tuple_list);
     free(_tuples_array);
+
+    return 0;
 
 }
 
